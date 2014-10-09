3a6e9ef4328c58cad89e6fa628124c86a75198bd
Andrew <andrewbranch@mail.com>
2014-10-09 07:33:32 -0500

    Initial Commit

---
+++ FUMCApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FUMCApp
+//
+//  Created by Andrew Branch on 10/9/14.
+//  Copyright (c) 2014 FUMC Pensacola. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
