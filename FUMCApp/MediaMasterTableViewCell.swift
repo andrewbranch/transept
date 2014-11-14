588e5be8550cb806340d25640ec15587723ae4cb
Andrew <andrewbranch@mail.com>
2014-11-14 12:26:54 -0600

    Redesign Media section. Happens to fix #3.

---
+++ FUMCApp/MediaMasterTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MediaMasterTableViewCell.swift
+//  FUMCApp
+//
+//  Created by Andrew Branch on 11/14/14.
+//  Copyright (c) 2014 FUMC Pensacola. All rights reserved.
+//
+
+import UIKit
+
+class MediaMasterTableViewCell: UITableViewCell {
+    
+    @IBOutlet var iconView: UIImageView?
+    @IBOutlet var label: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
