68f431ef543625a4b9744da84402ba42b647ba00
Andrew Branch <andrewbranch@mail.com>
2014-11-24 16:11:37 -0600

    Add Safari-like minimization of navigation bar while scrolling in web view. Fixes #4.

--- FUMCApp/MediaWebViewController.swift
+++ FUMCApp/MediaWebViewController.swift
@@ -3,21 +3,28 @@
 //  FUMCApp
 //
 //  Created by Andrew Branch on 10/30/14.
-//  Copyright (c) 2014 FUMC Pensacola. All rights reserved.
+//  Copyright (c) statusBarHeight14 FUMC Pensacola. All rights reserved.
 //
 
 import UIKit
 
-class MediaWebViewController: UIViewController, UIWebViewDelegate {
+class MediaWebViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate {
     
     @IBOutlet var webView: UIWebView?
     @IBOutlet var activityIndicator: UIActivityIndicatorView?
+    private var previousScrollViewYOffset = CGFloat(0)
     
     var url: NSURL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.webView!.delegate = self
+        self.webView!.scrollView.delegate = self
+        self.webView!.hidden = true
+        self.webView!.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+//        let insets = UIEdgeInsetsMake(self.navigationController!.navigationBar.frame.height + UIApplication.sharedApplication().statusBarFrame.height, 0, 0, 0)
+//        self.webView!.scrollView.contentInset = insets
+//        self.webView!.scrollView.scrollIndicatorInsets = insets
         
         if let url = self.url {
             var request = NSURLRequest(URL: url)
@@ -31,11 +38,71 @@ class MediaWebViewController: UIViewController, UIWebViewDelegate {
     
     func webViewDidFinishLoad(webView: UIWebView) {
         self.activityIndicator!.stopAnimating()
+        self.webView!.hidden = false
+//        self.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height + self.view.frame.origin.y)
+//        (self.navigationController!.navigationBar as GTScrollNavigationBar).scrollView = self.webView!.scrollView
+//        self.webView!.scrollView.setContentOffset(<#contentOffset: CGPoint#>, animated: <#Bool#>)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var frame = self.navigationController!.navigationBar.frame
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let size = frame.size.height - statusBarHeight;
+        let framePercentageHidden = ((statusBarHeight - frame.origin.y) / (frame.size.height - 1))
+        let scrollOffset = scrollView.contentOffset.y
+        let scrollDiff = scrollOffset - self.previousScrollViewYOffset
+        let scrollHeight = scrollView.frame.size.height
+        let scrollContentSizeHeight = scrollView.contentSize.height + scrollView.contentInset.bottom
+        
+        if (scrollOffset <= -scrollView.contentInset.top) {
+            frame.origin.y = statusBarHeight
+        } else if ((scrollOffset + scrollHeight) >= scrollContentSizeHeight) {
+            frame.origin.y = -size
+        } else {
+            frame.origin.y = min(statusBarHeight, max(-size, frame.origin.y - 4 * (frame.size.height * (scrollDiff / scrollHeight))))
+        }
+        
+        self.navigationController!.navigationBar.frame = frame
+        self.view.frame = CGRectMake(0, frame.maxY, self.view.frame.width, self.view.frame.height + self.view.frame.origin.y - frame.maxY)
+        
+        self.updateBarButtonItems(alpha: 1 - framePercentageHidden)
+        self.previousScrollViewYOffset = scrollOffset
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        stoppedScrolling()
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if (!decelerate) {
+            stoppedScrolling()
+        }
+    }
+    
+    func stoppedScrolling() {
+        let frame = self.navigationController!.navigationBar.frame
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        // Stopped in between
+        if (frame.origin.y < statusBarHeight && frame.origin.y > statusBarHeight - frame.height) {
+            animateNavBarTo(statusBarHeight)
+        }
+    }
+    
+    func updateBarButtonItems(#alpha: CGFloat) {
+        self.navigationItem.backBarButtonItem?.tintColor = self.navigationItem.backBarButtonItem?.tintColor.colorWithAlphaComponent(alpha)
+        self.navigationItem.titleView?.alpha = alpha
+        self.navigationController!.navigationBar.tintColor = self.navigationController!.navigationBar.tintColor.colorWithAlphaComponent(alpha)
+    }
+    
+    func animateNavBarTo(y: CGFloat) {
+        UIView.animateWithDuration(0.2) {
+            var frame = self.navigationController!.navigationBar.frame
+            let alpha = frame.origin.y >= y ? 0 : 1
+            frame.origin.y = y
+            self.navigationController!.navigationBar.frame = frame
+            self.updateBarButtonItems(alpha: CGFloat(alpha))
+        }
     }
 
 }
